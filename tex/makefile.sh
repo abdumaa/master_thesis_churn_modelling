de69119e6bca2311a8aff2258719846c63a4c0db
Abdu Maa <abdurahmanmaa@hotmail.de>
2021-11-20 12:02:53 +0100

    Introduction V1

--- tex/makefile.sh
+++ tex/makefile.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 cd /Users/abdumaa/Desktop/Uni_Abdu/Master/Masterarbeit/master_thesis_churn_modelling/
-
 # 1. Copy thesis.tex in new folder "tex/"
 mkdir tex/
 cp thesis.tex tex/thesis.tex
-
 # 2. Create pdf
 cd tex/
 pdflatex thesis.tex
-
 # 3. Copy pdf in thesis directory
 yes | cp -rf thesis.pdf ../thesis.pdf
-
 # 4. Clean up
 cd ..
 rm -r tex/
-
 cd /Users/abdumaa/Desktop/Uni_Abdu/Master/Masterarbeit/master_thesis_churn_modelling
